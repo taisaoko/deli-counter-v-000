74010cdbc50ab2da853c6fd05da510fab77bc17c
taisaoko <ttd3calvin@gmail.com>
2018-08-17 20:48:17 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def line(katz_deli)
+  case katz_deli.size
+  when 0
+  puts "The line is currently empty."
+  else 
+  puts "The line is currently: "
+  end
+end
+
+def take_a_number(katz_deli, name)
+  case katz_deli.size
+  when 0
+  num = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{num} in line"
+end
+
+def now_serving(katz_deli)
+  
