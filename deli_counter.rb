a0d1832e22db6a4543f8d0e16de28bfcd0bade85
taisaoko <ttd3calvin@gmail.com>
2018-08-18 19:37:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,18 +2,28 @@
 def line(katz_deli)
   case katz_deli.size
   when 0
-  puts "The line is currently empty."
+    puts "The line is currently empty."
   else 
-  puts "The line is currently: "
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
   end
 end
 
 def take_a_number(katz_deli, name)
-  case katz_deli.size
-  when 0
+  katz_deli.push(name)
   num = katz_deli.index(name) + 1
-  puts "Welcome, #{name}. You are number #{num} in line"
+  puts "Welcome, #{name}. You are number #{num} in line."
 end
 
 def now_serving(katz_deli)
-  
+  case katz_deli.size
+  when 0
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{katz_deli[0]}."
+    served = katz_deli.shift
+  end
+end
