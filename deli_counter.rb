8672be5e4945004abb50f762aee8d86f25b7fc0f
taisaoko <ttd3calvin@gmail.com>
2018-08-19 22:01:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,12 @@
 # Write your code here.
-require "pry"
 def line(katz_deli)
   case katz_deli.size
   when 0
     puts "The line is currently empty."
   else 
     current_line = "The line is currently:"
-    katz_deli.each.with_index(1) do |person, i|
+    katz_deli.each.with_index(0) do |person, i|
       current_line << " #{i}. #{person}"
-    binding.pry
     end
     puts current_line
   end
